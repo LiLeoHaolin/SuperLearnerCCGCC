cd25b1d4131bc2f296da34f659b0bb999382a086
Leo Haolin Li <88794190+LiLeoHaolin@users.noreply.github.com>
2023-11-05 23:31:12 -0500

    gcc

---
+++ GCC_high_lin/dat_gen.R
@@ -0,0 +1,118 @@
+## Prerequisites ##
+
+library(MASS)
+
+## Simulation setting ##
+
+#path.test <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/test/"
+#path.c <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/c/"
+#path.cc <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/cc/"
+#path.sc <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/sc/"
+#path.r <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/r/"
+
+path.test <- "/nas/longleaf/home/haolin/dissertation1/round5/data/test/"
+path.c <- "/nas/longleaf/home/haolin/dissertation1/round5/data/c/"
+path.cc <- "/nas/longleaf/home/haolin/dissertation1/round5/data/cc/"
+path.sc <- "/nas/longleaf/home/haolin/dissertation1/round5/data/sc/"
+path.r <- "/nas/longleaf/home/haolin/dissertation1/round5/data/r/"
+
+gamma.weibull = 0.7
+p = 400
+p.full = 140
+n = 1500
+q = 0.1
+q.comp = 0.154
+q.case = 0.3
+nsim = 500
+
+cenc = rep(0, nsim)
+
+## Data generation (training) ##
+
+for (j in 1:nsim){
+  cat(j)
+  
+  ar1_cor <- function(n, rho) {
+    exponent <- abs(matrix(1:n - 1, nrow = n, ncol = n, byrow = TRUE) - 
+                      (1:n - 1))
+    rho^exponent
+  }
+  
+  
+  X = as.matrix(mvrnorm(n = 2*n, matrix(0, nrow = p, ncol = 1), ar1_cor(p, 0.6)))
+  
+  beta = matrix(c(rep(0,p.full-2), c(0.1, 0.1, 0.3, -0.3, -0.2, -0.3, 0.01, -0.01, 0.25, -0.25), rep(0,p-p.full-8)), nrow=p)
+  
+  true.lin.pred <- X %*% beta +1.5 #+ 0.3*X[,1]*X[,3] + 0.2*X[,3]^2 + 0.2*X[,1]*X[,6]*X[,9]
+  v <- runif(n=2*n)
+  Tlat <- (- log(v) / (1* exp(true.lin.pred)))^(1 / gamma.weibull)
+ 
+  C = runif(2*n, min = 0, max = 0.03) # censoring times
+
+  end <- rep(0.0162, 2*n)
+  time <- pmin(Tlat, C, end) # follow-up times
+  
+  status <- as.numeric(Tlat == time) #event indicators
+  
+  
+  dat.pre = data.frame(X, status, time)
+  
+  dat = dat.pre[1:n,]
+  test = dat.pre[(n+1):(2*n),]
+
+  dat$subid = 1:n
+  test$subid = 1:n
+  
+  dat$sc = sample(c(1:0), n, replace=T, prob = c(q,1-q))
+  dat$r = sample(c(1:0), n, replace=T, prob = c(q.comp,1-q.comp))
+ 
+  cenc[j] = table(dat$status)[1]/n
+  
+  c <- subset(dat, select = -c(sc, r))
+  write.csv(c,file=paste0(path.c, 'dat', j, '.csv'), row.names = F)
+  
+  sc <- subset(dat, select = -c(r))
+  for (m in 1:n){
+    if (sc$sc[m]==1){
+      sc[m,c((p.full+1):p)] = sc[m,c((p.full+1):p)]
+    }else{
+      sc[m,c((p.full+1):p)] = NA
+    }
+  }
+  sc <- subset(sc, select = -c(sc))
+  write.csv(sc,file=paste0(path.sc, 'dat', j, '.csv'), row.names = F)
+  
+  r <- subset(dat, select = -c(sc))
+  for (m in 1:n){
+    if (r$r[m]==1){
+      r[m,c((p.full+1):p)] = r[m,c((p.full+1):p)]
+    }else{
+      r[m,c((p.full+1):p)] = NA
+    }
+  }
+  r <- subset(r, select = -c(r))
+  write.csv(r,file=paste0(path.r, 'dat', j, '.csv'), row.names = F)
+  
+  ntilde = sum(dat$sc)
+  
+  dat$sc.case = sample(c(1:0), n, replace=T, prob = c(q.case,1-q.case))
+  
+  cc <- subset(dat, select = -c(r))
+  for (m in 1:n){
+    if ((dat$sc[m]==1)|((dat$status[m]==1)&(dat$sc.case[m]==1))){
+      cc[m,c((p.full+1):p)] = cc[m,c((p.full+1):p)]
+    }else{
+      cc[m,c((p.full+1):p)] = NA
+    }
+  }
+  cc$weight = 1*(cc$status==1)*(cc$sc==1)+n/ntilde*(cc$status==0)*(cc$sc==1)+sum(dat$status*(1-dat$sc))/sum(dat$status*(1-dat$sc)*(dat$sc.case))*(cc$status==1)*(cc$sc==0)*(cc$sc.case==1)
+  cc <- subset(cc, select = -c(sc, sc.case))
+  #cc$weight = 1*(cc$status==1)+n/ntilde*(cc$status==0)
+  write.csv(cc,file=paste0(path.cc, 'dat', j, '.csv'), row.names = F)
+  
+  write.csv(test,file=paste0(path.test, 'dat', j, '.csv'), row.names = F)
+  
+}
+
+mean(cenc)
+
---
+++ GCC_high_lin/full_cohort.R
@@ -0,0 +1,354 @@
+## Prerequisites ##
+
+library(MASS)
+library(survival)
+library(pec)
+library(rms)
+library(randomForestSRC)
+library(alabama)
+library(CoxBoost)
+
+## Simulation setting ##
+
+#path.test <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/test/"
+#path.c <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/c/"
+#path.cc <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/cc/"
+#path.sc <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/sc/"
+#path.r <- "C:/Users/Haolin Li/Desktop/Dissertation/04_project1/07_simulation_new/data/r/"
+
+path.test <- "/nas/longleaf/home/haolin/dissertation1/round5/data/test/"
+path.c <- "/nas/longleaf/home/haolin/dissertation1/round5/data/c/"
+#path.cc <- "/nas/longleaf/home/haolin/dissertation1/round5/data/cc/"
+#path.r <- "/nas/longleaf/home/haolin/dissertation1/round5/data/r/"
+#path.sc <- "/nas/longleaf/home/haolin/dissertation1/round5/data/sc/"
+path.results <- "/nas/longleaf/home/haolin/dissertation1/round5/results/"
+
+p = 400
+p.full = 140
+nsim = 500
+batch = 1
+seq = ((batch-1)*nsim+1):(batch*nsim)
+tau = 0.0162
+fold = 10
+L = 2
+gamma.weibull = 0.7
+
+model = 'RC'
+spec = 'linear'
+cen.mod = 'ind'
+
+ipcw.wt.cum = matrix(0, nrow = nsim, ncol = L)
+
+cb.pa.cum = rep(0, nsim)
+srf.pa.cum = rep(0, nsim)
+ipcwsl.pa.cum = rep(0, nsim)
+
+
+for (m in seq){
+  cat(m)
+  try({
+  # 1. Prepare data for k-fold CV #
+  
+  dat = read.csv(paste0(path.c, 'dat', m, '.csv'))
+  n = nrow(dat)
+  #dat$subid = c(1:n)
+  dat$fold = 0
+  for (i in 1:n){
+    dat$fold[i] = sample(c(1:fold), 1)
+  }
+  
+  # 2. Model fitting of candidate learners and prediction for k-fold CV #
+  
+  # 2.1 CoxBoost model #
+  
+  cb.survival.pred = data.frame(subid = rep(0, n), t1 = rep(0, n), t2 = rep(0, n), t3 = rep(0, n), t4 = rep(0, n),
+                           t5 = rep(0, n), t6 = rep(0, n), t7 = rep(0, n), t8 = rep(0, n), t9 = rep(0, n))
+  for (j in 1:fold){
+    dat.train = dat[dat$fold!=j, ]
+    dat.test = dat[dat$fold==j, ]
+    cb.fit = CoxBoost(time=dat.train$time, status=dat.train$status, x=as.matrix(dat.train[,1:p])) 
+    for (i in 1:nrow(dat.test)){
+      subid = as.numeric(dat.test$subid[i])
+      cb.survival.pred$subid[subid] = subid
+      cb.survival.pred$t1[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 1*tau/10, type = "CIF")
+      cb.survival.pred$t2[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 2*tau/10, type = "CIF")
+      cb.survival.pred$t3[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 3*tau/10, type = "CIF")
+      cb.survival.pred$t4[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 4*tau/10, type = "CIF")
+      cb.survival.pred$t5[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 5*tau/10, type = "CIF")
+      cb.survival.pred$t6[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 6*tau/10, type = "CIF")
+      cb.survival.pred$t7[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 7*tau/10, type = "CIF")
+      cb.survival.pred$t8[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 8*tau/10, type = "CIF")
+      cb.survival.pred$t9[subid] = 1-predict(cb.fit, newdata = as.matrix(dat.test[i, 1:p]),  time = 9*tau/10, type = "CIF")
+    }
+  }
+
+  
+  # 2.2 Survival random forest #
+  
+  srf.survival.pred = data.frame(subid = rep(0, n), t1 = rep(0, n), t2 = rep(0, n), t3 = rep(0, n), t4 = rep(0, n),
+                            t5 = rep(0, n), t6 = rep(0, n), t7 = rep(0, n), t8 = rep(0, n), t9 = rep(0, n))
+  for (j in 1:fold){
+    dat.train = dat[dat$fold!=j, ]
+    #weight = dat.train$weight
+    dat.train <- subset(dat.train, select = -c(subid, fold))
+    dat.test = dat[dat$fold==j, ]
+    srf.fit = rfsrc(Surv(time, status) ~ ., data=dat.train)
+    for (i in 1:nrow(dat.test)){
+      srf.pred = data.frame(predict(srf.fit, newdata = dat.test[i,1:p])$survival[1,], predict(srf.fit, newdata = dat.test[i,1:p])$time.interest)
+      subid = as.numeric(dat.test$subid[i])
+      srf.pred$time1 = as.numeric(srf.pred[,2]-1*tau/10 <0)
+      srf.pred$time2 = as.numeric(srf.pred[,2]-2*tau/10 <0)
+      srf.pred$time3 = as.numeric(srf.pred[,2]-3*tau/10 <0)
+      srf.pred$time4 = as.numeric(srf.pred[,2]-4*tau/10 <0)
+      srf.pred$time5 = as.numeric(srf.pred[,2]-5*tau/10 <0)
+      srf.pred$time6 = as.numeric(srf.pred[,2]-6*tau/10 <0)
+      srf.pred$time7 = as.numeric(srf.pred[,2]-7*tau/10 <0)
+      srf.pred$time8 = as.numeric(srf.pred[,2]-8*tau/10 <0)
+      srf.pred$time9 = as.numeric(srf.pred[,2]-9*tau/10 <0)
+      srf.survival.pred$subid[subid] = subid
+      srf.survival.pred$t1[subid] = max(srf.pred[srf.pred$time1>0, 1])
+      srf.survival.pred$t2[subid] = max(srf.pred[srf.pred$time2>0, 1])
+      srf.survival.pred$t3[subid] = max(srf.pred[srf.pred$time3>0, 1])
+      srf.survival.pred$t4[subid] = max(srf.pred[srf.pred$time4>0, 1])
+      srf.survival.pred$t5[subid] = max(srf.pred[srf.pred$time5>0, 1])
+      srf.survival.pred$t6[subid] = max(srf.pred[srf.pred$time6>0, 1])
+      srf.survival.pred$t7[subid] = max(srf.pred[srf.pred$time7>0, 1])
+      srf.survival.pred$t8[subid] = max(srf.pred[srf.pred$time8>0, 1])
+      srf.survival.pred$t9[subid] = max(srf.pred[srf.pred$time9>0, 1])
+    }
+  }
+  
+  # 3. IPCW Super Learner #
+  
+  # 3.1 Indicators #
+  
+  ind = dat
+  ind$t1 = as.numeric((ind$time > 1*tau/10))
+  ind$t2 = as.numeric((ind$time > 2*tau/10))
+  ind$t3 = as.numeric((ind$time > 3*tau/10))
+  ind$t4 = as.numeric((ind$time > 4*tau/10))
+  ind$t5 = as.numeric((ind$time > 5*tau/10))
+  ind$t6 = as.numeric((ind$time > 6*tau/10))
+  ind$t7 = as.numeric((ind$time > 7*tau/10))
+  ind$t8 = as.numeric((ind$time > 8*tau/10))
+  ind$t9 = as.numeric((ind$time > 9*tau/10))
+  ind = subset(ind, select = c(subid, t1, t2, t3, t4, t5, t6, t7, t8, t9))
+  
+  # 3.2 Fraction in the front #
+  
+  cen.fit <- survfit(Surv(time, (1-status)) ~ 1 , data=dat)
+  #cent.pred = summary(cen.fit, times = 0.01)$surv
+  
+  denom = dat
+  denom$time1 = (as.numeric(denom$time <= 1*tau/10)*denom$time + as.numeric(denom$time > 1*tau/10)*1*tau/10) 
+  denom$time2 = (as.numeric(denom$time <= 2*tau/10)*denom$time + as.numeric(denom$time > 2*tau/10)*2*tau/10) 
+  denom$time3 = (as.numeric(denom$time <= 3*tau/10)*denom$time + as.numeric(denom$time > 3*tau/10)*3*tau/10) 
+  denom$time4 = (as.numeric(denom$time <= 4*tau/10)*denom$time + as.numeric(denom$time > 4*tau/10)*4*tau/10) 
+  denom$time5 = (as.numeric(denom$time <= 5*tau/10)*denom$time + as.numeric(denom$time > 5*tau/10)*5*tau/10) 
+  denom$time6 = (as.numeric(denom$time <= 6*tau/10)*denom$time + as.numeric(denom$time > 6*tau/10)*6*tau/10) 
+  denom$time7 = (as.numeric(denom$time <= 7*tau/10)*denom$time + as.numeric(denom$time > 7*tau/10)*7*tau/10) 
+  denom$time8 = (as.numeric(denom$time <= 8*tau/10)*denom$time + as.numeric(denom$time > 8*tau/10)*8*tau/10) 
+  denom$time9 = (as.numeric(denom$time <= 9*tau/10)*denom$time + as.numeric(denom$time > 9*tau/10)*9*tau/10) 
+  
+  denom$cen.pred1 = 0
+  denom$cen.pred2 = 0
+  denom$cen.pred3 = 0
+  denom$cen.pred4 = 0
+  denom$cen.pred5 = 0
+  denom$cen.pred6 = 0
+  denom$cen.pred7 = 0
+  denom$cen.pred8 = 0
+  denom$cen.pred9 = 0
+  for (i in 1:n){
+    denom$cen.pred1[i] = summary(cen.fit, times = denom$time1[i])$surv
+    denom$cen.pred2[i] = summary(cen.fit, times = denom$time2[i])$surv
+    denom$cen.pred3[i] = summary(cen.fit, times = denom$time3[i])$surv
+    denom$cen.pred4[i] = summary(cen.fit, times = denom$time4[i])$surv
+    denom$cen.pred5[i] = summary(cen.fit, times = denom$time5[i])$surv
+    denom$cen.pred6[i] = summary(cen.fit, times = denom$time6[i])$surv
+    denom$cen.pred7[i] = summary(cen.fit, times = denom$time7[i])$surv
+    denom$cen.pred8[i] = summary(cen.fit, times = denom$time8[i])$surv
+    denom$cen.pred9[i] = summary(cen.fit, times = denom$time9[i])$surv
+  }
+  
+  denom$t1 = (1-as.numeric((denom$time <= 1*tau/10)&(denom$status == 0)))/denom$cen.pred1
+  denom$t2 = (1-as.numeric((denom$time <= 2*tau/10)&(denom$status == 0)))/denom$cen.pred2
+  denom$t3 = (1-as.numeric((denom$time <= 3*tau/10)&(denom$status == 0)))/denom$cen.pred3
+  denom$t4 = (1-as.numeric((denom$time <= 4*tau/10)&(denom$status == 0)))/denom$cen.pred4
+  denom$t5 = (1-as.numeric((denom$time <= 5*tau/10)&(denom$status == 0)))/denom$cen.pred5
+  denom$t6 = (1-as.numeric((denom$time <= 6*tau/10)&(denom$status == 0)))/denom$cen.pred6
+  denom$t7 = (1-as.numeric((denom$time <= 7*tau/10)&(denom$status == 0)))/denom$cen.pred7
+  denom$t8 = (1-as.numeric((denom$time <= 8*tau/10)&(denom$status == 0)))/denom$cen.pred8
+  denom$t9 = (1-as.numeric((denom$time <= 9*tau/10)&(denom$status == 0)))/denom$cen.pred9
+  
+  frac = subset(denom, select = c(subid, t1, t2, t3, t4, t5, t6, t7, t8, t9))
+  
+  # 3.3 Compute IPCW SL weights #
+  
+  fn <- function(x){
+    fn = sum((as.matrix(ind[,-1])-(x[1]*as.matrix(cb.survival.pred[,-1]) + x[2]*as.matrix(srf.survival.pred[,-1])))^2 * (as.matrix(frac[,-1]))) / (n)
+    fn
+  } 
+  
+  heq <- function(x) {
+    h <- rep(NA, 1)
+    h[1] <- x[1] + x[2] - 1
+    h
+  }
+  
+  hin <- function(x) {
+    h <- rep(NA, 1)
+    h[1] <- x[1]
+    h[2] <- x[2]
+    h
+  }
+  
+  set.seed(12)
+  p0 <- runif(2)
+  ans <- constrOptim.nl(par=p0, fn=fn, heq=heq, hin=hin)
+  ipcw.wt = ans$par
+  
+  ipcw.wt.cum[m,] = ipcw.wt
+  
+  
+  # 5. Prediction Accuracy for candidate learners and super learner #
+  
+  # 5.0 Calculate the true survival probability #
+  dat.pa = read.csv(paste0(path.test, 'dat', m, '.csv'))
+  
+  beta = matrix(c(rep(0,p.full-2), c(0.1, 0.1, 0.3, -0.3, -0.2, -0.3, 0.01, -0.01, 0.25, -0.25), rep(0,p-p.full-8)), nrow=p)
+  true.lin.pred <- as.matrix(dat.pa[,1:p]) %*% beta +1.5 #+ 0.3*X[,1]*X[,3] + 0.2*X[,3]^2 + 0.2*X[,1]*X[,6]*X[,9]
+  
+  survival.true <- function(t){
+    survival =  exp(-exp(true.lin.pred)*t^gamma.weibull)
+    return(survival)
+  }
+  true.survival = data.frame(subid = c(1:nrow(dat.pa)), t1 = rep(0, nrow(dat.pa)), t2 = rep(0, nrow(dat.pa)), 
+                        t3 = rep(0, nrow(dat.pa)), t4 = rep(0, nrow(dat.pa)), t5 = rep(0, nrow(dat.pa)), 
+                        t6 = rep(0, nrow(dat.pa)), t7 = rep(0, nrow(dat.pa)), t8 = rep(0, nrow(dat.pa)), 
+                        t9 = rep(0, nrow(dat.pa)), t10 = rep(0, nrow(dat.pa)), t11 = rep(0, nrow(dat.pa)),
+                        t12 = rep(0, nrow(dat.pa)), t13 = rep(0, nrow(dat.pa)), t14 = rep(0, nrow(dat.pa)))
+  true.survival$t1 = survival.true(1*tau/15)
+  true.survival$t2 = survival.true(2*tau/15)
+  true.survival$t3 = survival.true(3*tau/15)
+  true.survival$t4 = survival.true(4*tau/15)
+  true.survival$t5 = survival.true(5*tau/15)
+  true.survival$t6 = survival.true(6*tau/15)
+  true.survival$t7 = survival.true(7*tau/15)
+  true.survival$t8 = survival.true(8*tau/15)
+  true.survival$t9 = survival.true(9*tau/15)
+  true.survival$t10 = survival.true(10*tau/15)
+  true.survival$t11 = survival.true(11*tau/15)
+  true.survival$t12 = survival.true(12*tau/15)
+  true.survival$t13 = survival.true(13*tau/15)
+  true.survival$t14 = survival.true(14*tau/15)
+  
+  # 5.1 CoxBoost model #
+  
+  cb.survival.pa = data.frame(subid = rep(0, nrow(dat.pa)), t1 = rep(0, nrow(dat.pa)), t2 = rep(0, nrow(dat.pa)), 
+                         t3 = rep(0, nrow(dat.pa)), t4 = rep(0, nrow(dat.pa)), t5 = rep(0, nrow(dat.pa)), 
+                         t6 = rep(0, nrow(dat.pa)), t7 = rep(0, nrow(dat.pa)), t8 = rep(0, nrow(dat.pa)), 
+                         t9 = rep(0, nrow(dat.pa)), t10 = rep(0, nrow(dat.pa)), t11 = rep(0, nrow(dat.pa)),
+                         t12 = rep(0, nrow(dat.pa)), t13 = rep(0, nrow(dat.pa)), t14 = rep(0, nrow(dat.pa)))
+  cb.fit.pa = CoxBoost(time=dat$time, status=dat$status, x=as.matrix(dat[,1:p])) 
+  for (i in 1:nrow(dat.pa)){
+    subid = as.numeric(dat.pa$subid[i])
+    cb.survival.pa$subid[subid] = subid
+    cb.survival.pa$t1[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 1*tau/15, type = "CIF")
+    cb.survival.pa$t2[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 2*tau/15, type = "CIF")
+    cb.survival.pa$t3[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 3*tau/15, type = "CIF")
+    cb.survival.pa$t4[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 4*tau/15, type = "CIF")
+    cb.survival.pa$t5[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 5*tau/15, type = "CIF")
+    cb.survival.pa$t6[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 6*tau/15, type = "CIF")
+    cb.survival.pa$t7[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 7*tau/15, type = "CIF")
+    cb.survival.pa$t8[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 8*tau/15, type = "CIF")
+    cb.survival.pa$t9[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 9*tau/15, type = "CIF")
+    cb.survival.pa$t10[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 10*tau/15, type = "CIF")
+    cb.survival.pa$t11[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 11*tau/15, type = "CIF")
+    cb.survival.pa$t12[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 12*tau/15, type = "CIF")
+    cb.survival.pa$t13[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 13*tau/15, type = "CIF")
+    cb.survival.pa$t14[subid] = 1-predict(cb.fit.pa, newdata = as.matrix(dat.pa[i, 1:p]),  time = 14*tau/15, type = "CIF")
+  }
+  
+  cb.pa <- sum((as.matrix(cb.survival.pa[,-1]) - as.matrix(true.survival[,-1]))^2)/nrow(dat.pa)/14
+  cb.pa.cum[m] = cb.pa
+  
+  
+  
+  # 5.2 Survival random forest #
+  
+  #weight = dat$weight
+  dat.learn <- subset(dat, select = -c(subid, fold))
+  
+  srf.survival.pa = data.frame(subid = rep(0, nrow(dat.pa)), t1 = rep(0, nrow(dat.pa)), t2 = rep(0, nrow(dat.pa)), 
+                          t3 = rep(0, nrow(dat.pa)), t4 = rep(0, nrow(dat.pa)), t5 = rep(0, nrow(dat.pa)), 
+                          t6 = rep(0, nrow(dat.pa)), t7 = rep(0, nrow(dat.pa)), t8 = rep(0, nrow(dat.pa)), 
+                          t9 = rep(0, nrow(dat.pa)), t10 = rep(0, nrow(dat.pa)), t11 = rep(0, nrow(dat.pa)),
+                          t12 = rep(0, nrow(dat.pa)), t13 = rep(0, nrow(dat.pa)), t14 = rep(0, nrow(dat.pa)))
+  srf.fit <-  rfsrc(Surv(time, status) ~ ., data = dat.learn)
+    for (i in 1:nrow(dat.pa)){
+      srf.pred = data.frame(predict(srf.fit, newdata = dat.pa[i,1:p])$survival[1,], predict(srf.fit, newdata = dat.pa[i,1:p])$time.interest)
+      subid = as.numeric(dat.pa$subid[i])
+      srf.pred$time1 = as.numeric(srf.pred[,2]-1*tau/15 <0)
+      srf.pred$time2 = as.numeric(srf.pred[,2]-2*tau/15 <0)
+      srf.pred$time3 = as.numeric(srf.pred[,2]-3*tau/15 <0)
+      srf.pred$time4 = as.numeric(srf.pred[,2]-4*tau/15 <0)
+      srf.pred$time5 = as.numeric(srf.pred[,2]-5*tau/15 <0)
+      srf.pred$time6 = as.numeric(srf.pred[,2]-6*tau/15 <0)
+      srf.pred$time7 = as.numeric(srf.pred[,2]-7*tau/15 <0)
+      srf.pred$time8 = as.numeric(srf.pred[,2]-8*tau/15 <0)
+      srf.pred$time9 = as.numeric(srf.pred[,2]-9*tau/15 <0)
+      srf.pred$time10 = as.numeric(srf.pred[,2]-10*tau/15 <0)
+      srf.pred$time11 = as.numeric(srf.pred[,2]-11*tau/15 <0)
+      srf.pred$time12 = as.numeric(srf.pred[,2]-12*tau/15 <0)
+      srf.pred$time13 = as.numeric(srf.pred[,2]-13*tau/15 <0)
+      srf.pred$time14 = as.numeric(srf.pred[,2]-14*tau/15 <0)
+      srf.survival.pa$subid[subid] = subid
+      srf.survival.pa$t1[subid] = max(srf.pred[srf.pred$time1>0, 1])
+      srf.survival.pa$t2[subid] = max(srf.pred[srf.pred$time2>0, 1])
+      srf.survival.pa$t3[subid] = max(srf.pred[srf.pred$time3>0, 1])
+      srf.survival.pa$t4[subid] = max(srf.pred[srf.pred$time4>0, 1])
+      srf.survival.pa$t5[subid] = max(srf.pred[srf.pred$time5>0, 1])
+      srf.survival.pa$t6[subid] = max(srf.pred[srf.pred$time6>0, 1])
+      srf.survival.pa$t7[subid] = max(srf.pred[srf.pred$time7>0, 1])
+      srf.survival.pa$t8[subid] = max(srf.pred[srf.pred$time8>0, 1])
+      srf.survival.pa$t9[subid] = max(srf.pred[srf.pred$time9>0, 1])
+      srf.survival.pa$t10[subid] = max(srf.pred[srf.pred$time10>0, 1])
+      srf.survival.pa$t11[subid] = max(srf.pred[srf.pred$time11>0, 1])
+      srf.survival.pa$t12[subid] = max(srf.pred[srf.pred$time12>0, 1])
+      srf.survival.pa$t13[subid] = max(srf.pred[srf.pred$time13>0, 1])
+      srf.survival.pa$t14[subid] = max(srf.pred[srf.pred$time14>0, 1])
+  }
+  
+  srf.pa <- sum((as.matrix(srf.survival.pa[,-1]) - as.matrix(true.survival[,-1]))^2)/nrow(dat.pa)/14
+  srf.pa.cum[m] = srf.pa
+  
+  # 5.6 IPCW super learner #
+  
+  ipcwsl.survival.pa = ipcw.wt[1]*cb.survival.pa[,-1] + ipcw.wt[2]*srf.survival.pa[,-1]
+  ipcwsl.pa <- sum((as.matrix(ipcwsl.survival.pa) - as.matrix(true.survival[,-1]))^2)/nrow(dat.pa)/14
+  ipcwsl.pa.cum[m] = ipcwsl.pa
+  })
+}
+
+
+write.csv(ipcw.wt.cum, file=paste0(path.results, 'c_large_ipcw_wt_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+write.csv(ipcwsl.pa.cum, file=paste0(path.results, 'c_large_ipcwsl_pa_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+
+write.csv(cb.pa.cum, file=paste0(path.results, 'c_large_cb_pa_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+write.csv(srf.pa.cum, file=paste0(path.results, 'c_large_srf_pa_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+
+#write.csv(cb.pa.cum, file=paste0(path.results, 'cc_w_cb_pa_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+#write.csv(srf.pa.cum, file=paste0(path.results, 'cc_w_srf_pa_', model, '_', spec, '_', cen.mod, '_', batch, '.csv'), row.names = F)
+
+
+
+         
+
+
+
+
+
+
+
+
